d38222ac8747b1fbc97b57ec424afd0b467ad043
Justin Poutoa <jpoutoa865@gmail.com>
2024-05-06 01:29:55 +1200

    Added basically nothing useful

--- MakeBMTable.java
+++ MakeBMTable.java
@@ -1,14 +1,27 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
 public class MakeBMTable {
     public static void main(String[] args){
         if(args.length != 2){
-            System.err.println("Usage: java MakeBMTable <input_text> <table_file_path>");
+            System.err.println("Usage: java MakeBMTable <input_text> <output_file_path>");
             System.exit(1);
         }
 
         String inputString = args[0];
         String tableFilePath = args[1];
+
+        int stringLength = inputString.length();
+        System.out.println("Length of input word: " + stringLength);
+
+        ArrayList<String> textArray = new ArrayList<String>();
+        textArray.add(inputString);
+
+        //Given a pattern string and its length, construct two arrays
+        //Bad character heuristic array (BC)
+        
+
     }
 }
