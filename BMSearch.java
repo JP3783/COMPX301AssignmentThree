e01e313396b13eddab84afacb95f813e781db4c5
William <William@DESKTOP-UAAQGI8>
2024-05-09 14:52:04 +1200

    Added skeleton methods to BMSearch - William

--- BMSearch.java
+++ BMSearch.java
@@ -9,5 +9,22 @@ public class BMSearch {
         // Parse command-line arguments
         String skipArrayFile = args[0];
         String textFile = args[1];
+
+        // Read and store skip array file
+        int[][] skipArray = readSkipArray(skipArrayFile);
+
+        // Search for target string
+        searchInText(textFile, skipArray);
+    }
+
+    // Function to read skip array file and return skip array
+    private static int[][] readSkipArray(String skipArrayFile) {
+        int[][] skipArray = null;
+        return skipArray;
+    }
+
+    // Function to search for target string in text file using skip array
+    private static void searchInText(String textFile, int[][] skipArray) {
+
     }
 }
