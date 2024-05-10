983bed500bc73afe74d9435c008d809dedbc06d9
William <William@DESKTOP-UAAQGI8>
2024-05-10 14:48:47 +1200

    added funcionality to construct skip array and visualize it in the console - WIlliam

--- MakeBMTable.java
+++ MakeBMTable.java
@@ -1,30 +1,53 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * 
  */
 public class MakeBMTable {
-    public static void main(String[] args){
-        //Command-line Argument Validation
-        if(args.length != 2){
+    public static void main(String[] args) {
+        // Command-line Argument Validation
+        if (args.length != 2) {
             System.err.println("Usage: java MakeBMTable <input_text> <output_file_path>");
             System.exit(1);
         }
 
-        //Reading Command-line Arguments
+        // Reading Command-line Arguments
         String inputString = args[0];
         String tableFilePath = args[1];
 
-        int stringLength = inputString.length();
-        System.out.println("Length of input word: " + stringLength);
-        
-        //Construction of Skip Array
-        ArrayList<String> textArray = new ArrayList<String>();
-        textArray.add(inputString);
+        // Compute BM skip array
+        int[] skipArray = constructSkipArray(inputString);
 
-        //Given a pattern string and its length, construct two arrays
-        //Bad character heuristic array (BC)
-        
+        // Write skip array to file
+        writeSkipArrayToFile(skipArray, inputString, tableFilePath);
+    }
 
+    // Function to construct skip array and visualize it in the console
+    private static int[] constructSkipArray(String inputString) {
+        int[] skipArray = new int[256];
+        Arrays.fill(skipArray, inputString.length());
+
+        // Populate skip array
+        for (int i = 0; i < inputString.length(); i++) {
+            skipArray[inputString.charAt(i)] = inputString.length() - i - 1;
+        }
+
+        // Print skip array
+        System.out.println("Skip Array:");
+        for (int i = 0; i < 256; i++) {
+            if (skipArray[i] != inputString.length()) {
+                char c = (char) i;
+                System.out.println("Character: " + c + ", Skip Distance: " + skipArray[i]);
+            }
+        }
+
+        return skipArray;
+    }
+    // Function to write skip array to file
+    private static void writeSkipArrayToFile(int[] skipArray, String inputString, String filePath) {
     }
 }
